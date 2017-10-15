61f4bea08846a6345acdbb9b3a6f681a577e4096
3lv27 <elvin.gm27@gmail.com>
2017-10-15 17:41:24 +0200

    version b. 0.1

--- rover.js
+++ rover.js
@@ -1,25 +1,109 @@
+// This is Rover
 var myRover = {
-  position: [0,0], 
+  position: [0,0],
   direction: 'N'
 };
 
+
+//The functions for its movements
 function goForward(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.position[0]++
+      rover.position[0]++;
       break;
     case 'E':
-      rover.position[1]++
+      rover.position[1]++;
       break;
     case 'S':
-      rover.position[0]--
+      rover.position[0]--;
       break;
     case 'W':
-      rover.position[1]--
+      rover.position[1]--;
       break;
   };
 
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
 }
 
-goForward(myRover);
+
+function goBackwards(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]--;
+      break;
+    case 'E':
+      rover.position[1]--;
+      break;
+    case 'S':
+      rover.position[0]++;
+      break;
+    case 'W':
+      rover.position[1]++;
+      break;
+  };
+
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+}
+
+function turnRight(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'E';
+      break;
+    case 'E':
+      rover.direction = 'S';
+      break;
+    case 'S':
+      rover.direction = 'W';
+      break;
+    case 'W':
+      rover.direction = 'N';
+      break;
+  };
+
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+  console.log(`Rover now is facing to the ${rover.direction}`);
+}
+
+function turnLeft(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'W';
+      break;
+    case 'E':
+      rover.direction = 'N';
+      break;
+    case 'S':
+      rover.direction = 'E';
+      break;
+    case 'W':
+      rover.direction = 'S';
+      break;
+  };
+
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+  console.log(`Rover now is facing to the ${rover.direction}`);
+}
+
+
+//The commands
+
+let input = document.getElementById('direction');
+
+function commands() {
+  switch (input.value) {
+    case 'f':
+    goForward(myRover);
+        break;
+    case 'b':
+    goBackwards(myRover);
+        break;
+    case 'l':
+    turnLeft(myRover);
+        break;
+    case 'r':
+    turnRight(myRover);
+        break;
+  }
+
+}
